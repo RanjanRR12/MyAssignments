0912a53ae8414460847fec5779c3728b14ae9c80
RanjanRR12 <117539070+RanjanRR12@users.noreply.github.com>
2022-11-15 03:06:21 +0530

    Add files via upload

---
+++ day2/Calculator.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Calculator {
+	
+	public void add(int num1,int num2,int num3) {
+		// TODO Auto-generated method stub
+		System.out.println(num1+num2+num3);
+
+	}
+	
+	public void sub(int num1,int num2) {
+		// TODO Auto-generated method stub
+		System.out.println(num1-num2);
+
+	}
+	
+	public void mul(double num1,double num2) {
+		// TODO Auto-generated method stub
+		System.out.println(num1*num2);
+
+	}
+	
+	public void divide(float num1,float num2) {
+		// TODO Auto-generated method stub
+		System.out.println(num1/num2);
+
+	}
+	
+
+}
---
+++ day2/Mycalculator.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class Mycalculator {
+	public static void main(String[] args) {
+		Calculator Arith=new Calculator();
+		Arith.add(10,20,30);
+		Arith.sub(30,20);
+		Arith.mul(10.59,12.57);
+		Arith.divide(30,20);
+	}
+
+}
